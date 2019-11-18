4301bdef84908cab4d05c531a0e852a83b95dbec
Justin Swiger <justin.swiger@me.com>
2019-11-18 10:05:12 -0500

    TODO fix js

--- public/main.js
+++ public/main.js
@@ -1,7 +1,26 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+const qs = e => document.querySelector(e)
+
+// const addToList = () => {
+//   const getInput = document.getElementById('stringList').value
+//   const userInput = document.getElementById('input').value
+//   const createItem = document.createElement('li')
+//   createItem.appendChild(document.createTextNode(userInput))
+//   createItem.appendChild(getInput)
+//   console.log(createItem)
+// }
+
+{
+  /* <ol id="demo"></ol>
+and get a reference to it: */
 }
 
-document.addEventListener('DOMContentLoaded', main)
+var list = document.getElementById('demo')
+
+// In your event handler, create a new list element with the input value as content and append to the list with Node.appendChild [MDN]:
+
+var firstname = document.getElementById('firstname').value
+var entry = document.createElement('li')
+entry.appendChild(document.createTextNode(firstname))
+list.appendChild(entry)
+
+qs('.search').addEventListener('click', addToList)
